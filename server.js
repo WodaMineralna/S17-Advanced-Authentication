5ab8c21e59872e055a0b18733952656395f8fc0e
WodaMineralna <raknieborak65@gmail.com>
2025-10-05 22:11:56 +0200

    refactor(server): read SERVER_PORT from env with fallback to 3000

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ require("dotenv").config();
 const app = require("./app");
 const { mongoConnect } = require("./src/db/database");
 
+const required = require("./utils/requireEnvVar")
+
+const PORT = required("SERVER_PORT") || 3000;
+
 // ^ setting up MongoDB connection
 mongoConnect(() => {
-  app.listen(3000);
+  app.listen(PORT);
 });
